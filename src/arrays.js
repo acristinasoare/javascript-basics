18afbfd176611902697e8746cfa04a08e6491d14
Cristina Soare <kiti.soare@ymail.com>
2023-04-29 19:45:27 +0100

    solved part of arrays functions'

--- src/arrays.js
+++ src/arrays.js
@@ -1,34 +1,63 @@
 const getNthElement = (index, array) => {
-  // your code here
-};
+  return array [index >= array.length ? index % array.length : index]
+}
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.join();
+  
+  //creates a new string from an array by concatanating all the elements of the array; by default it 
+  //uses a comma as a separator, but you can specify what separators you want to be added between the items. eg join() - comma but no space, 
+  //join(, ), join (-)
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',')  
+  
+  // string.split(seperator) --> if no seperator specified, will return the string as one element inside an array
+  //if separator specified, for example space ' ' or comma ','  this is where split method will divide the string
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  return array.concat(element); 
 };
+
+//allows you to merge two or more arrays into a new array which will be returned, 
+  //can also be used as a push method, so add one element to the end of the array without modifying the original array/
+
+// alternartive solution 
+// newArray = array.slice()   
+// newArray.push(element)
+// return newArray
+
+// -slice returns a shallow copy of a portion of an array - 
+//which means it does not modify the original array; if no arguments given to function,
+// by default the starting index will be 0, and the ending index is array.length
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1)
+  return array;
 };
+
+// .splice () changes an array by removing or replacing existing elements 
+// parameters - (start, deleteCount, item1,...,item n)
+// start - index at which to start changing the array
+// deleteCount - integer indicating the number of elements to remove from start
+// the elements to add to the array beginning from start
 
 const numbersToStrings = numbers => {
-  // your code here
+ return numbers.map(number =>number.toString())
 };
 
+// map creates a new array populated with the results of calling a function to every element
+//in the calling array
+
 const uppercaseWordsInArray = strings => {
-  // your code here
-};
+ return strings.map(string => string.toUpperCase())
+}
 
 const reverseWordsInArray = strings => {
   // your code here
